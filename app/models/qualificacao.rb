987ad41c7346a2c72cf43e23760c0c4aad7bab3f
Robson Fernandes <robfds@gmail.com>
2016-05-15 19:04:09 -0300

    Association validation on qualificacao

--- app/models/qualificacao.rb
+++ app/models/qualificacao.rb
@@ -5,6 +5,8 @@ class Qualificacao < ActiveRecord::Base
     less_than_or_equals_to: 10, message: "Deve ser um número entre 0 e 10"
     validates_numericality_of :valor_gasto, greather_than: 0,
     message: "Valor gasto deve ser maior do que 0"
+    validates_presence_of :cliente_id, :restaurante_id
+    validates_associated :cliente, :restaurante
 
     # Associations
     belongs_to :restaurante
