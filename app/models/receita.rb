58e6e91d10d21b5b0e4e118ff1a94b3ebe60e584
Robson Fernandes <robfds@gmail.com>
2016-05-15 19:09:04 -0300

    Association validation on receita

--- app/models/receita.rb
+++ app/models/receita.rb
@@ -1,5 +1,7 @@
 class Receita < ActiveRecord::Base
     validates_presence_of :conteudo, "Não pode ficar em branco"
+    validates_presence_of :prato_id
+    validates_associated :prato
 
     # Associations
     belongs_to :prato
