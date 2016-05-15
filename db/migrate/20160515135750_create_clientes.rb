57d3f0bdd6cf4d6f240c67cbfae2f44ae3c420ad
Robson Fernandes <robfds@gmail.com>
2016-05-15 12:03:26 -0300

    Defining atributes data_nascimento and nome of Cliente as mandatory on bd level

--- db/migrate/20160515135750_create_clientes.rb
+++ db/migrate/20160515135750_create_clientes.rb
@@ -1,8 +1,8 @@
 class CreateClientes < ActiveRecord::Migration
   def change
     create_table :clientes do |t|
-      t.string :nome
-      t.date :data_nascimento
+      t.string :nome, null: false
+      t.date :data_nascimento, null: false
 
       t.timestamps null: false
     end
