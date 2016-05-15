cd4c485df0763bb5feaa794b4c9174b9b81a0d82
Robson Fernandes <robfds@gmail.com>
2016-05-15 18:22:00 -0300

    Adding the limitation of 50 characters (on the database level) to the customer's name.

--- db/migrate/20160515135750_create_clientes.rb
+++ db/migrate/20160515135750_create_clientes.rb
@@ -1,7 +1,7 @@
 class CreateClientes < ActiveRecord::Migration
   def change
     create_table :clientes do |t|
-      t.string :nome, null: false
+      t.string :nome, limit: 50, null: false
       t.date :data_nascimento, null: false
 
       t.timestamps null: false
