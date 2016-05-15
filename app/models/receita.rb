1abae7f69f65234c7489b2139a2c212591fe4c0e
Robson Fernandes <robfds@gmail.com>
2016-05-15 20:48:47 -0300

    Fix small sintaxy error

--- app/models/receita.rb
+++ app/models/receita.rb
@@ -1,5 +1,5 @@
 class Receita < ActiveRecord::Base
-    validates_presence_of :conteudo, "Não pode ficar em branco"
+    validates_presence_of :conteudo, message: "Não pode ficar em branco"
     validates_presence_of :prato_id
     validates_associated :prato
 
