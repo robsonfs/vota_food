e1a5e8b53e2ca2a4ede18506a172bf16193891f3
Robson Fernandes <robfds@gmail.com>
2016-05-15 17:54:31 -0300

    Fix bug on Cliente validation: An exception was generated when trying to call the validation 'nascidos_apos_o_ano_2000' on a date of birth in white.

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,14 +1,15 @@
 class Cliente < ActiveRecord::Base
-    validates_presence_of :nome, :data_nascimento message: "Deve ser preenchido"
-    validates_length_of :nome, :maximum 50,
-    message: "Não pode ter mais de 50 caracteres"
     validate :nascidos_apos_o_ano_2000
+    validates_presence_of :nome, :data_nascimento, message: "Deve ser preenchido"
+    validates_length_of :nome, maximum: 50, message: "Não pode ter mais de 50 caracteres"
 
     private
-    def nascidos_apos_o_ano_2000:
+    def nascidos_apos_o_ano_2000
         message_error = "Data de nascimento deve ser maior do que 31/12/1999"
-        errors.add(
-            :data_nascimento, message_error
-        ) unless data_nascimento < Date.new(1999,12,31)
+        if data_nascimento
+            errors.add(
+                "data_nascimento", message_error
+            ) unless data_nascimento > Date.new(1999,12,31)
+        end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515044055) do
+ActiveRecord::Schema.define(version: 20160515135750) do
+
+  create_table "clientes", force: :cascade do |t|
+    t.string   "nome",            limit: 255, null: false
+    t.date     "data_nascimento",             null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "pratos", force: :cascade do |t|
     t.string   "nome",       limit: 255, null: false
