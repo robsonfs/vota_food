3b260cb36c7c646bd4b1826b439c72b0f29e2133
Robson Fernandes <robfds@gmail.com>
2016-05-15 21:21:55 -0300

    run db:migrate for comentario

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515215254) do
+ActiveRecord::Schema.define(version: 20160516000913) do
 
   create_table "clientes", force: :cascade do |t|
     t.string   "nome",            limit: 255, null: false
     t.date     "data_nascimento",             null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+  end
+
+  create_table "comentarios", force: :cascade do |t|
+    t.text     "conteudo",   limit: 65535
+    t.string   "author",     limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "pratos", force: :cascade do |t|
